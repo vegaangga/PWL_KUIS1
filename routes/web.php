9afceb0fd39f262dbf5aed44bb59d22a47293ed3
vegaangga <vegaangga27@gmail.com>
2021-03-11 00:42:48 +0700

    Admin_make_route_data

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\DataController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use Symfony\Component\VarDumper\Cloner\Data;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +31,7 @@ use Illuminate\Support\Facades\Route;
 //Praktikum 4 Model - Database
 
 Route::get('/',[HomeController::class, 'index']);
+Route::get('/data',[DataController::class, 'data']);
 Route::get('/about',[AboutController::class, 'about']);
 Route::get('/profile',[ProfileController::class, 'profile']);
 Route::get('/contact',[ContactController::class, 'contact']);
