2a430006dc8a188240c4bf3e071655b348cd63e2
vegaangga <vegaangga27@gmail.com>
2021-03-11 00:30:32 +0700

    Admin_make_pegawai_controller

---
+++ database/migrations/2021_03_10_160537_create_pegawais_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePegawaisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pegawai', function (Blueprint $table) {
+            $table->bigIncrements('pegawai_id');
+            $table->string('pegawai_name');
+            $table->string('pegawai_phone');
+            $table->string('pegawai_email');
+            $table->text('pegawai_address');
+            $table->string('pegawai_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pegawai');
+    }
+}
