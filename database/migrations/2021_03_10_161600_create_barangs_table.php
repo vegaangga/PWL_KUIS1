e43ebbf8df9bb5624f568cd40bd35466958ef78c
vegaangga <vegaangga27@gmail.com>
2021-03-11 00:37:44 +0700

    Admin_make_barang_model

---
+++ database/migrations/2021_03_10_161600_create_barangs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->bigIncrements('barang_id');
+            $table->string('barang_name');
+            $table->integer('barang_beli');
+            $table->integer('barang_jual');
+            $table->integer('barang_stock');
+            $table->string('barang_image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
