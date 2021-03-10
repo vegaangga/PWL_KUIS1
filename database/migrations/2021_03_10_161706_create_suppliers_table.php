c6ada87658056313b91bd8d6057ce7f01e606d8c
vegaangga <vegaangga27@gmail.com>
2021-03-11 00:35:47 +0700

    Admin_make_supplier_model

---
+++ database/migrations/2021_03_10_161706_create_suppliers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('supplier', function (Blueprint $table) {
+            $table->bigIncrements('supplier_id');
+            $table->string('supplier_name');
+            $table->string('supplier_phone');
+            $table->string('supplier_email');
+            $table->text('supplier_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
