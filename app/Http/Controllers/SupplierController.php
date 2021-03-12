453dbb3ee08b06e4546ccd63700daea672ed48d6
ChikaLabita <itaaa.c2@gmail.com>
2021-03-12 20:06:27 +0700

    Supplier Controller

--- app/Http/Controllers/SupplierController.php
+++ app/Http/Controllers/SupplierController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\supplier;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SupplierController extends Controller
 {
-    //
+    public function supplier(){
+    
+        $data = supplier::paginate(2);
+        return view('supplier', ['supplier' => $data]);
+        }
 }
