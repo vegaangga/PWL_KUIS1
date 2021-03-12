e2f47a62a792b1fe59ab26fe5a23acae4c0e86f8
ChikaLabita <itaaa.c2@gmail.com>
2021-03-12 20:33:43 +0700

    Supplier Seeder

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -16,64 +16,64 @@ class SupplierSeeder extends Seeder
     {
         $data_supplier = [
             [
-                'supplier_name'=>'Fenita Aurora',
-                'supplier_phone'=>'(0240)-00115483',
-                'supplier_email'=>'FenitaA@gmail.com',
-                'supplier_address'=>'Jln. A. Yani, Malang'
+                'supplier_name'=>'Venisya Aurora Putri',
+                'supplier_phone'=>'(0240)-00115888',
+                'supplier_email'=>'VPutriA@gmail.com',
+                'supplier_address'=>'Jln. A. Yani, Surakarta'
             ],
         
             [
-                'supplier_name'=>'Rizky Ardian',
+                'supplier_name'=>'Ricky Ha',
                 'supplier_phone'=>'(0348)-88445140',
-                'supplier_email'=>'RArdrian_1@gmail.com',
-                'supplier_address'=>'Jln. Kemangi, Pasuruan'
+                'supplier_email'=>'RrickyHaa@gmail.com',
+                'supplier_address'=>'Jln. Semanggi, Pasuruan'
             ],
 
             [
-                'supplier_name'=>'Reyna Azizah',
-                'supplier_phone'=>'(0345)-55441203',
-                'supplier_email'=>'ReyAzizah@gmail.com',
-                'supplier_address'=>'Jln. Anggreka2, Bekasi'
+                'supplier_name'=>'Gerry',
+                'supplier_phone'=>'(0345)-55444532',
+                'supplier_email'=>'Gerry_01@gmail.com',
+                'supplier_address'=>'Jln. Anggrek Kencana, Bekasi'
             ],
 
             [
-                'supplier_name'=>'Debyan Firmansyah',
-                'supplier_phone'=>'(0343)-90456210',
-                'supplier_email'=>'Dfirman_syah@gmail.com',
-                'supplier_address'=>'Jln. Setia Budi, Jakarta Selatan'
+                'supplier_name'=>'Dessy Ratna',
+                'supplier_phone'=>'(0343)-90453250',
+                'supplier_email'=>'DesyYa@gmail.com',
+                'supplier_address'=>'Jln. Setia Budi, Yogyakarta'
             ],
 
             [
-                'supplier_name'=>'Santy Febrianty',
-                'supplier_phone'=>'(0245)-55802121',
-                'supplier_email'=>'SantyAnty@gmail.com',
-                'supplier_address'=>'Jln. Diponegoro, Pemalang'
+                'supplier_name'=>'Yanti Safitri',
+                'supplier_phone'=>'(0245)-55821421',
+                'supplier_email'=>'Yanty@gmail.com',
+                'supplier_address'=>'Jln. Diponegoro, Surabaya'
             ],
 
             [
-                'supplier_name'=>'Chandra Putra',
-                'supplier_phone'=>'(0345)-55841020',
-                'supplier_email'=>'CPutra11@gmail.com',
-                'supplier_address'=>'Jln. Bunga Cokelat, Tasikmalaya'
+                'supplier_name'=>'Cherly',
+                'supplier_phone'=>'(0345)-55800020',
+                'supplier_email'=>'Cherly@gmail.com',
+                'supplier_address'=>'Jln. Bunga Cokelat, Jakarta Utara'
             ],
 
             [
-                'supplier_name'=>'Qodru Andi Santosa',
-                'supplier_phone'=>'(0343)-81547896',
-                'supplier_email'=>'qas_santos@gmail.com',
-                'supplier_address'=>'Jln. Anggrek, Surabaya'
+                'supplier_name'=>'M. Sholeh',
+                'supplier_phone'=>'(0343)-90157896',
+                'supplier_email'=>'Sholeh@gmail.com',
+                'supplier_address'=>'Jln. Anggrek, Surakarta'
             ],
 
             [
-                'supplier_name'=>'Annisa Dwi',
-                'supplier_phone'=>'(02930-88741500)',
-                'supplier_email'=>'Dwi_Annisa23@gmail.com',
-                'supplier_address'=>'Jln. Bumi Indah, Wonosari'
+                'supplier_name'=>'Dwiyana',
+                'supplier_phone'=>'(02930-88731400)',
+                'supplier_email'=>'Dwi_y@gmail.com',
+                'supplier_address'=>'Jln. Bumi Indah, Wonorejo'
             ],
 
             [
-                'supplier_name'=>'Firman Adriansyah',
-                'supplier_phone'=>'(0343)-00154801',
+                'supplier_name'=>'Very',
+                'supplier_phone'=>'(0343)-00159941',
                 'supplier_email'=>'Firmanadri@gmail.com',
                 'supplier_address'=>'Jln. Pangeran Antasari, Jakarta Selatan'
             ],
