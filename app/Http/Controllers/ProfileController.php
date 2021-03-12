7ac55cbbb8433a01138c07f916810a378ba339b9
ChikaLabita <itaaa.c2@gmail.com>
2021-03-12 23:40:15 +0700

    Profile Controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,12 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\profile;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProfileController extends Controller
 {
     public function profile(){
-        return view('profile');
-    }
+
+        $data = profile::paginate(10);
+        $header = profile::where('profile_id', 21)->first();
+        return view('profile', ['profile' => $data], ['header' => $header]);
+        }
 }
