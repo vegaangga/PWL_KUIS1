beddaef741642d43c6d5ce8dc6ffee3f32161ebc
ChikaLabita <itaaa.c2@gmail.com>
2021-03-12 20:03:36 +0700

    Pelanggan Controller

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\pelanggan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PelangganController extends Controller
 {
-    //
+    public function pelanggan(){
+    
+    $data = pelanggan::paginate(2);
+    
+    return view('pelanggan', ['pelanggan' => $data]);
+    }
 }
