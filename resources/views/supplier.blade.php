73d35073dfdf51b754f90da6afd4cf50ffcda556
ChikaLabita <itaaa.c2@gmail.com>
2021-03-12 20:40:02 +0700

    Supplier Blade

---
+++ resources/views/supplier.blade.php
@@ -0,0 +1,36 @@
+@extends('table.data')
+
+@section ('content_table')
+
+<div id="folio-container" class="row">
+    <h2 id="data">Data Supplier</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Phone</th>
+          <th>Email</th>
+          <th>Address</th>
+        </tr>
+      </thead>
+      <tbody>
+          @foreach ($supplier as $item)
+        <tr>
+          <td>{{ $item->supplier_name }}</td>
+          <td>{{ $item->supplier_phone }}</td>
+          <td>{{ $item->supplier_email }}</td>
+          <td>{{ $item->supplier_address }}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+    <div class="d-flex justify-content-center">
+        {{ $supplier->links() }}
+    </div>
+
+</div>
+</div>
+
+
+@endsection
