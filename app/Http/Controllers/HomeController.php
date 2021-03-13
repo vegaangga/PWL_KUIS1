401704667ed73aab5ed4641e2eda444c33b95e59
vegaangga <vegaangga27@gmail.com>
2021-03-14 01:51:55 +0700

    home_final

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,11 +10,20 @@ class HomeController extends Controller
 {
     public function index(){
 
-        $data = Home::where('home_id',1)->first();
+
         $header = Home::where('home_id',2)->first();
         $slider = Home::where('home_id',3)->first();
+        $image = Home::where('home_title','image_slider')->first();
+        $we = Home::where('home_id',4)->first();
 
-        return view ('home',['slider' => $slider],['header' => $header]);
+
+        //return view ('home',['slider' => $slider],['header' => $header],['data' => $data]);
+
+        return view('home')
+            ->with('slider', $slider)
+            ->with('header', $header)
+            ->with('data', $image)
+            ->with('we',$we);
         /*$alldata = Home::all();
         return view ('home', ['data' => $alldata]);
         */
--- app/Models/Home.php
+++ app/Models/Home.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Home extends Model
 {
     use HasFactory;
+
+    protected $table = 'homes';
 }
