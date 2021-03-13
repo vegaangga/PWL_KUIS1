708f4725f115a5ff23dd77d60e86deab4e2be2c5
vegaangga <vegaangga27@gmail.com>
2021-03-14 00:14:12 +0700

    pegawai_table_changed

--- resources/views/pegawai.blade.php
+++ resources/views/pegawai.blade.php
@@ -7,23 +7,26 @@
     <table class="table">
       <thead>
         <tr>
+          <th>No.</th>
           <th>Name</th>
           <th>Phone</th>
           <th>Email</th>
           <th>Address</th>
-          <th>Picture</th>
           <th>Position</th>
+          <th>Picture</th>
         </tr>
       </thead>
       <tbody>
-          @foreach ($pegawai as $item)
+
+          @foreach ($pegawai as $index => $item)
         <tr>
+          <td>{{ $index + $pegawai->firstItem() }}</td>
           <td>{{ $item->pegawai_name }}</td>
           <td>{{ $item->pegawai_phone }}</td>
           <td>{{ $item->pegawai_email }}</td>
           <td>{{ $item->pegawai_address }}</td>
-          <td><img src="../../img/{{ $item->pegawai_image}}" style="width: 75px;"></td>
           <td>{{ $item->pegawai_position }}</td>
+          <td><img src="../../img/{{ $item->pegawai_image}}" style="width: 75px;"></td>
         </tr>
         @endforeach
       </tbody>
